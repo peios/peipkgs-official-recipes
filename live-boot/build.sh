15d4af6386c16996a550bda0d8dd3475fe82cdf2
Jack Palfrey <jackgpalfrey@outlook.com>
2026-06-08 05:28:02 +0100

    feat: add live boot kernel cmdline

--- live-boot/build.sh
+++ live-boot/build.sh
@@ -4,3 +4,4 @@ set -eu
 cd "$SOURCE_DIR"
 mkdir -p "$DESTDIR/boot/initramfs/hooks"
 cp live-boot/mount-root.sh "$DESTDIR/boot/initramfs/hooks/mount-root.sh"
+install -D -m 0644 live-boot/cmdline "$DESTDIR/boot/cmdline"
